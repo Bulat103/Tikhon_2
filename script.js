d90adfa9a83ed266cef762c2a3e0b0b778c2d9d9
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2024-07-01 18:04:18 +0300

    сначала рендерятся превью, при клике на картинку встраивается либо видео либо ифрейм, при клике на другое превью предыдущее заменяется снова на картинку - превью

---
+++ script.js
@@ -0,0 +1,288 @@
+const items = [
+  {
+    id: "68",
+    preview: "https://musicreel.moscow/pics/rzd.jpg",
+    video: "https://player.vimeo.com/video/915310027?h=d272ad52ce",
+    tags: "rzd uprising music epic beats synth string orchestral cinematic",
+    width: 200,
+    height: 200,
+    format: "vimeo",
+  },
+  {
+    id: "1",
+    preview: "https://musicreel.moscow/pics/baltika/baltika_7.jpg",
+    video: "https://musicreel.moscow/vids/baltika_7.mov",
+    tags: "other music guitar rock dark vocal foley soundfx cinematic",
+    width: 300,
+    height: 200,
+    format: "mp4",
+  },
+  {
+    id: "2",
+    preview: "https://musicreel.moscow/pics/casio_gshock.jpg",
+    video: "https://player.vimeo.com/video/710819339?h=4e09a7b8f5",
+    tags: "casio piano music orchestal string epic cinematic uprising",
+    width: 300,
+    height: 200,
+    format: "vimeo",
+  },
+  {
+    id: "3",
+    preview: "https://musicreel.moscow/pics/zg/zg_3mugs.jpg",
+    video: "https://musicreel.moscow/vids/zg_3mugs.mov",
+    tags: "orchestal string epic cinematic",
+    width: 300,
+    height: 200,
+    format: "mov",
+  },
+  {
+    id: "4",
+    preview: "https://musicreel.moscow/pics/zarina/zarina_20.jpg",
+    video: "https://www.youtube.com/embed/u_OTfAIx1GU",
+    tags: "zarina music beats songs vocal hip-hop",
+    width: 300,
+    height: 200,
+    format: "youtube",
+  },
+];
+
+// -----------------------------------------------------------------------
+const windowWidth = window.innerWidth;
+let scale = 1;
+if (windowWidth > 1500) {
+  scale = 2.2;
+} else if (windowWidth > 1200) {
+  scale = 1.6;
+} else if (windowWidth > 900) {
+  scale = 1.3;
+}
+
+// -----------------------------------------------------------------------
+const createPlayBtn = () => {
+  const btn = document.createElement("button");
+  btn.classList.add("start_play");
+  btn.innerText = "Play";
+  return btn;
+};
+
+// -----------------------------------------------------------------------
+const createImg = (item) => {
+  const img = document.createElement("img");
+  img.setAttribute("src", item.preview);
+  img.setAttribute("width", item.width * scale);
+  img.setAttribute("height", item.height * scale);
+  img.setAttribute("id", `video_${item.id}`);
+  img.setAttribute("class", "preview_img");
+  return img;
+};
+
+// -----------------------------------------------------------------------
+const replaceVideoWithImg = (currentVideo) => {
+  if (!currentVideo) return;
+
+  const videos = document.querySelectorAll("video");
+  const iframes = document.querySelectorAll("iframe");
+
+  let playerItems;
+
+  if (iframes.length) {
+    playerItems = iframes;
+  } else if (videos.length) {
+    playerItems = videos;
+  } else {
+    return;
+  }
+
+  let prevVideo;
+  playerItems.forEach((el) => {
+    const id = el.getAttribute("id");
+    if (id != currentVideo.id) {
+      prevVideo = el;
+    }
+  });
+  const prevVideoParent = prevVideo.parentElement;
+  let parentId = prevVideoParent.getAttribute("id").split("_")[1];
+  const elem = items.find((item) => item.id === parentId);
+  const img = createImg(elem);
+  prevVideo.replaceWith(img);
+  const btn = createPlayBtn();
+  prevVideoParent.append(btn);
+};
+
+const videoFormats = ["mov", "mp4"];
+
+// -----------------------------------------------------------------------
+const replaceImgWithVideo = (item) => {
+  const imgContainer = document.querySelector(`#video_${item.id}`);
+  const img = imgContainer.querySelector(`img`);
+  const btn = imgContainer.querySelector(`button`);
+  let videoElement;
+  if (videoFormats.some((el) => item.format === el)) {
+    videoElement = document.createElement("video");
+    videoElement.setAttribute("controls", true);
+    videoElement.setAttribute("autoplay", true);
+  } else {
+    videoElement = document.createElement("iframe");
+    videoElement.setAttribute("frameborder", "0");
+    videoElement.setAttribute("allow", "autoplay");
+    videoElement.setAttribute("allowfullscreen", true);
+  }
+
+  videoElement.setAttribute("src", item.video);
+  videoElement.setAttribute("width", item.width * scale);
+  videoElement.setAttribute("height", item.height * scale);
+  replaceVideoWithImg(item);
+  img.replaceWith(videoElement);
+  btn.remove();
+};
+
+// -----------------------------------------------------------------------
+const renderPreview = (videos) => {
+  const vidoesContainer = document.querySelector(".vidoes_container");
+  const renderPreviewImage = (item, number) => {
+    const container = document.createElement("div");
+    container.setAttribute("class", "preview_img_container");
+    container.setAttribute("data-tags", item.tags);
+    container.setAttribute("id", `video_${item.id}`);
+    container.setAttribute("data-number", number);
+    container.style.width = item.width;
+    container.style.height = item.height;
+    const img = createImg(item);
+    const btn = createPlayBtn();
+    container.append(img);
+    container.append(btn);
+    container.addEventListener("click", () => replaceImgWithVideo(item));
+    vidoesContainer.append(container);
+  };
+
+  for (let i = 0; i < videos.length; i++) {
+    renderPreviewImage(videos[i], i);
+  }
+};
+renderPreview(items);
+// -----------------------------------------------------------------------
+const hidePreloader = () => {
+  const preloader = document.querySelector(".preloader");
+  preloader.classList.add("hidden");
+};
+
+// -----------------------------------------------------------------------
+function resetVideos(videoItems) {
+  videoItems.forEach((videoItem) => {
+    videoItem.style.display = "block";
+  });
+}
+
+// -----------------------------------------------------------------------
+const changeFooterAboutInfoVisibility = () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+
+  if (scrollTop + clientHeight >= scrollHeight - 20) {
+    footer.classList.add("visible");
+  } else {
+    footer.classList.remove("visible");
+  }
+};
+
+// -----------------------------------------------------------------------
+const onDocumentLoadedHandler = () => {
+  const videoItems = document.querySelectorAll(".preview_img_container");
+  const tags = document.querySelectorAll("[data-tag]");
+  const resetTagsButton = document.querySelector(".reset-tags");
+
+  // hidePreloader вызывается только когда загружены все внешние ресурсы
+  hidePreloader();
+
+  function filterVideos() {
+    const selectedTags = Array.from(tags).filter((tag) =>
+      tag.classList.contains("selected")
+    );
+    videoItems.forEach((videoItem) => {
+      const videoTags = videoItem.getAttribute("data-tags").split(" ");
+      let matchCount = 0;
+      selectedTags.forEach((tag) => {
+        if (videoTags.includes(tag.getAttribute("data-tag"))) {
+          matchCount++;
+        }
+      });
+      if (matchCount === selectedTags.length) {
+        videoItem.style.display = "block";
+      } else {
+        videoItem.style.display = "none";
+      }
+    });
+    // Отключаем выбранные теги, если они не соответствуют видео
+    tags.forEach((tag) => {
+      if (!selectedTags.includes(tag) && !tag.classList.contains("disabled")) {
+        const tagVideos = Array.from(videoItems).filter((videoItem) =>
+          videoItem
+            .getAttribute("data-tags")
+            .split(" ")
+            .includes(tag.getAttribute("data-tag"))
+        );
+        if (tagVideos.every((video) => video.style.display === "none")) {
+          tag.classList.add("disabled");
+        }
+      }
+    });
+  }
+
+  function toggleTag(tag) {
+    if (tag.classList.contains("selected")) {
+      tag.classList.remove("selected");
+    } else if (
+      Array.from(tags).filter((tag) => tag.classList.contains("selected"))
+        .length < 8
+    ) {
+      tag.classList.add("selected");
+    }
+    filterVideos();
+  }
+
+  function restoreDisabledTags() {
+    tags.forEach((tag) => {
+      if (tag.classList.contains("disabled")) {
+        const tagVideos = Array.from(videoItems).filter((videoItem) =>
+          videoItem
+            .getAttribute("data-tags")
+            .split(" ")
+            .includes(tag.getAttribute("data-tag"))
+        );
+        if (tagVideos.some((video) => video.style.display !== "none")) {
+          tag.classList.remove("disabled");
+        }
+      }
+    });
+  }
+
+  tags.forEach((tag) => {
+    tag.addEventListener("click", function () {
+      toggleTag(tag);
+      restoreDisabledTags();
+    });
+  });
+
+  resetTagsButton.addEventListener("click", function () {
+    tags.forEach((tag) => {
+      tag.classList.remove("selected");
+      tag.classList.remove("disabled");
+    });
+    resetVideos(videoItems);
+  });
+};
+
+// -----------------------------------------------------------------------
+window.onload = onDocumentLoadedHandler;
+
+const footer = document.querySelector("footer");
+
+window.addEventListener("scroll", changeFooterAboutInfoVisibility);
+
+document.querySelector(".feedback_btn").addEventListener("click", () => {
+  footer.classList.add("visible");
+});
+
+// если раскомментить эту строчку и закомментить строку №34-вызов функции hidePreloader, то лоадер будет исчезать сразу после того как браузер построит каркас страницы, но не обязательно загрузит все внешние ресурсы
+// document.addEventListener("DOMContentLoaded", hidePreloader);
